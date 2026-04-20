54d5249266c097109c608121b7e142e6d6211955
CesarArias28 <cesararias2420@gmail.com>
2026-04-20 11:15:24 +0000

    relationship_adding

--- src/models.py
+++ src/models.py
@@ -24,12 +24,15 @@ class User(db.Model):
 
         }
     
-class User(db.Model):
+class Character(db.Model):
     __tablename__ = 'characters'
     id: Mapped[int] = mapped_column(primary_key=True)
     name_characeter:  Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     gender: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     hair: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    favorites: Mapped[list["Favorite"]] = relationship(back_populate="characters")
+
 
     def serialize(self):
         return {
@@ -40,12 +43,15 @@ class User(db.Model):
         }
 
 
-class Planets(db.Model):
+class Planet (db.Model):
     __tablename__ = 'planets'
     id: Mapped[int] = mapped_column(primary_key=True)
     name:  Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     climate: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     population : Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    favorites: Mapped[list["Favorite"]] = relationship(back_populate="planet")
+
 
 def serialize(self):
         return {
@@ -60,10 +66,14 @@ class Favorite(db.Model):
     __tablename__ = 'favorite'
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
-    planet_id:  Mapped[str] = mapped_column(ForeignKey("planet.id"), unique=True, nullable=False)
-    character_id:  Mapped[str] = mapped_column(ForeignKey("character.id"), unique=True, nullable=False)
+    planet_id:  Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    character_id:  Mapped[int] = mapped_column(ForeignKey("character.id"),nullable=True)
 
-    user: Mapped["User"]= relationship(back_populates="favorites")
+    
+    planet = relationship(back_populates="favorites")
+    character = relationship(back_populates="favorites")
+    user = relationship(back_populates="favorites")
+
 
     def serialize(self):
         return {
