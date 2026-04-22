119a1f82c3af054a4c07221ed246bd9d6776648c
CesarArias28 <cesararias2420@gmail.com>
2026-04-22 09:27:17 +0000

    update_modelado_de_datos_final_

--- src/app.py
+++ src/app.py
@@ -9,12 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Character, Planet, Favorite
-from flask_jwt_extended import (
-    JWTManager,
-    jwt_required,
-    create_access_token,
-    get_jwt_identity
-)
+from sqlalchemy import select
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -40,67 +35,6 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-
-@app.route('/login', methods=['POST'])
-
-def login():
-
- body = request.get_json()
- 
-   
-
-
-
-@app.route('/planets/<int:planet_id>', methods=['GET'])
-def get_one_planet(planet_id):
-
-    single_planet = Planet.query.get(planet_id)
-
-    if single_planet is None:
-
-        return jsonify({"msg": "Planet not found"}), 404
-    
-    response_body = single_planet.serialize()
-    return jsonify(response_body), 200
-
-
-
-@app.route('/users/<int:user_id>/favorites', methods=['GET'])
-
-def get_all_favorites(user_id):
-
-    all_favorites = Favorite.query.filter_by(user_id=user_id).all()
-
-    all_favorites = [a.serialize() for a in all_favorites]
-
-    return jsonify(all_favorites), 200 
-
-@app.route('/people', methods=['GET'])
-
-def get_all_characters():
-
-    all_characters = Character.query.all()
-
-    character_list = [c.serialize() for c in all_characters]
-
-    return jsonify(character_list), 200 
-
-@app.route('/planets/<int:planet_id>', methods=['DELETE'])
-def delete_planet(planet_id):
-
-    single_planet = Planet.query.get(planet_id)
-
-    if single_planet is None:
-        return jsonify({"msg": "Planet not found"}), 404
-
-    db.session.delete(single_planet)
-    db.session.commit()
-
-    return jsonify({"msg": "Planet deleted successfully"}), 200
-
-
-
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
